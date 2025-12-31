20c65f229ab7761fd380afdabf57fca2711f6ac3
DeftSlyNinja <christopher_masson@hotmail.com>
2025-12-30 16:44:47 -0800

    finished

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from stats import get_book_text, get_word_count, get_character_counts, report_stats
-
 frankenstein = "./books/frankenstein.txt"
 
 def main():
     frankText = get_book_text("./books/frankenstein.txt")
-    # print(frankText)
-    # get_word_count(frankText)
     frankChars = get_character_counts(frankText)
-    # print(frankChars)
     sorted_chars = report_stats(frankChars)
+    # Print the program title
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {frankenstein}")
+    # Print Word Count Header
+    print("----------- Word Count ----------")
+    # Print Word Count
+    print(f"Found {get_word_count(frankText)} total words")
+    # Print Character Count Header
+    print("--------- Character Count -------")
+    # Print Print list of characters & their counts
     for item in sorted_chars:
-        print(item)
+        print(f"{item['char']}: {item['num']}")
+    # Print END
+    print("============= END ===============")
 
 main()
