0bf7cbb19cfe21e72d7ec0958dfa8be9cb6df5df
DeftSlyNinja <christopher_masson@hotmail.com>
2025-12-31 16:23:08 -0800

    add sys.argv so it can be used with other books

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+import sys
 from stats import get_book_text, get_word_count, get_character_counts, report_stats
-frankenstein = "./books/frankenstein.txt"
 
 def main():
-    frankText = get_book_text("./books/frankenstein.txt")
-    frankChars = get_character_counts(frankText)
-    sorted_chars = report_stats(frankChars)
+    if len(sys.argv) == 2:
+        book = sys.argv[1]
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    bookText = get_book_text(book)
+    bookChars = get_character_counts(bookText)
+    sorted_chars = report_stats(bookChars)
     # Print the program title
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {frankenstein}")
+    print(f"Analyzing book found at {book}")
     # Print Word Count Header
     print("----------- Word Count ----------")
     # Print Word Count
-    print(f"Found {get_word_count(frankText)} total words")
+    print(f"Found {get_word_count(bookText)} total words")
     # Print Character Count Header
     print("--------- Character Count -------")
     # Print Print list of characters & their counts
